f33619bc7f37f2c8dbf798ff478930c928cec07c
Le Tuan Khoi <93958255+lekhoi0702@users.noreply.github.com>
2026-03-16 11:51:34 +0700

    feat: simple in-memory todo console

--- todo.ConsoleApp/Program.cs
+++ todo.ConsoleApp/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+var todos = new List<string>();
+while (true)
+{
+    Console.WriteLine("\"1) Add todo  2) List todos  3) Exit\"");
+    var input = Console.ReadLine();
+    if (input == "1")
+    {
+        Console.Write("Task:");
+        var task = Console.ReadLine();
+        todos.Add(task);
+        Console.WriteLine("added");
+    }
+    else if (input == "2")
+    {
+        Console.WriteLine("Todos:");
+        foreach (var todo in todos)
+        {
+            Console.WriteLine(todo);
+        }
+    }
+    else if (input == "3")
+    {
+        break;
+    }
+}
